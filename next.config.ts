381000e04ff3727bcfe0eb21147c7cc5536848cd
ImDipper <admin@vidlecta.com>
2026-01-25 14:48:13 +0300

    Enable standalone output in next.config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "standalone",
 };
 
 export default nextConfig;
